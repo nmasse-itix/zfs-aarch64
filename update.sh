8b626f58b5fe1e8b21e272c798e27bae0880f730
Nicolas MASSE <nicolas.masse@itix.fr>
2025-10-30 09:18:21 +0100

    fix typo in update.sh

--- update.sh
+++ update.sh
@@ -10,7 +10,9 @@ for v in 41 42 43; do
   echo "Downloading libvirt source for Fedora $v..."
   dnf download --repofrompath=$dist,https://dl.fedoraproject.org/pub/fedora/linux/releases/$v/Everything/source/tree/ --repofrompath=$dist-updates,https://dl.fedoraproject.org/pub/fedora/linux/updates/$v/Everything/source/tree/ --repo="$dist" --repo="$dist-updates" --source libvirt --destdir $dist/SRPMS/
   echo "Downloading zfs source for Fedora $v..."
-  dnf download --repofrompath=zfs-$dist,http://download.zfsonlinux.org/fedora/42/SRPMS/ --repo=zfs-$dist --source zfs zfs-dkms --destdir $dist/SRPMS/
+  if ! dnf download --repofrompath=zfs-$dist,http://download.zfsonlinux.org/fedora/$v/SRPMS/ --repo=zfs-$dist --source zfs zfs-dkms --destdir $dist/SRPMS/; then
+    echo "ZFS source RPM not found for Fedora $v, skipping..."
+  fi
   echo "Importing source RPMs for Fedora $v..."
   rpm -ivh "--define=_topdir $PWD/$dist" $dist/SRPMS/*.src.rpm
 done
@@ -21,7 +23,9 @@ for v in 9 10; do
   echo "Downloading libvirt source for CentOS Stream $v..."
   dnf download --repofrompath=$dist,https://mirror.stream.centos.org/$v-stream/AppStream/source/tree/ --repo="$dist" --source libvirt --destdir $dist/SRPMS/
   echo "Downloading zfs source for CentOS Stream $v..."
-  dnf download --repofrompath=zfs-$dist,http://download.zfsonlinux.org/epel/10/SRPMS/ --repo=zfs-$dist --source zfs zfs-dkms --destdir $dist/SRPMS/
+  if ! dnf download --repofrompath=zfs-$dist,http://download.zfsonlinux.org/epel/$v/SRPMS/ --repo=zfs-$dist --source zfs zfs-dkms --destdir $dist/SRPMS/; then
+    echo "ZFS source RPM not found for CentOS Stream $v, skipping..."
+  fi
   echo "Importing source RPMs for CentOS Stream $v..."
   rpm -ivh "--define=_topdir $PWD/$dist" $dist/SRPMS/*.src.rpm
 done
